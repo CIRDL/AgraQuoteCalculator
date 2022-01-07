c14a256dd1eecf5efbc977bb75fc304e8fd0a404
Cesar Ramirez <81725973+CIRDL@users.noreply.github.com>
2022-01-07 10:38:31 -0600

    Finished order_class.py

--- order_class.py
+++ order_class.py
@@ -1,4 +1,5 @@
 from calculations import *
+import math
 
 
 class Order:
@@ -8,17 +9,30 @@ class Order:
         self.diameter_tank = diameter_tank
         self.diameter_liner = self.diameter_tank + 1
         self.depth_tank = depth_tank
+        self.depth_liner = depth_tank + 1
         self.total_liners = 1
+        self.discounted = False
+        self.discounted_single_liner_cost = 0
+        # Order.get_square_footage(self) * self.square_footage_price
+        self.lifting_hem_cost = 0
+
+    def get_single_lining_system(self):
+        single_lining_sys = Order.get_single_liner_cost(self) + Order.get_batten_strip_cost(self) \
+                            + Order.get_lifting_hem_cost(self) + Order.get_jbolt_cost(self) \
+                            + Order.get_single_layer_geo_cost(self, "both")
+        return single_lining_sys
+
+    def get_total_lining_system(self):
+        return Order.get_single_lining_system(self) * self.total_liners
 
     def __get_floor_square_footage(self):
         floor_square_footage = self.diameter_liner ** 2
         return round(floor_square_footage)
 
     def __get_wall_square_footage(self):
-        side_wall_square_footage = Order.get_liner_circumference(self) * self.depth_tank
+        side_wall_square_footage = Order.get_liner_circumference(self) * self.depth_liner
         return round(side_wall_square_footage)
 
-    # Legit square footage without 5% or foot added
     def get_actual_square_footage(self, section):
         if section[0].lower() == "f":
             return Order.__get_floor_square_footage(self)
@@ -30,7 +44,9 @@ class Order:
     # Square footage used to charge stuff
     def get_square_footage(self):
         five_percent = Order.get_actual_square_footage(self, "total") * 0.05
-        square_footage = Order.get_actual_square_footage(self, "total") + five_percent
+        if self.lifting_hem_cost > 0:
+            five_percent = (Order.get_actual_square_footage(self, "total") + Order.get_liner_circumference(self)) * 0.05
+        square_footage = math.ceil(Order.get_actual_square_footage(self, "total") + five_percent)
         return round(square_footage)
 
     # Check legit calculations before charging stuff
@@ -44,11 +60,13 @@ class Order:
         return circumference_tank
 
     def get_liner_circumference(self):
-        circumference_tank = self.diameter_liner * math.pi
-        return circumference_tank
+        circumference_liner = self.diameter_liner * math.pi
+        return circumference_liner
 
     def get_single_liner_cost(self):
         liner_cost = Order.get_square_footage(self) * self.square_footage_price
+        if self.discounted:
+            liner_cost = Order.__get_liner_discount(self)
         return liner_cost
 
     def get_total_liners_cost(self):
@@ -71,15 +89,14 @@ class Order:
         else:
             return geo_wall_cost + geo_floor_cost
 
-    # Don't know if multiple layers will be necessary yet
-    def get_total_geo_cost(self):
-        raise NotImplementedError
-
     def get_jbolt_cost(self):
         jbolt_cost = 20
-        jbolt_num = math.ceil(Order.get_tank_circumference(self) / 1.5)
+        jbolt_num = Order.get_jbolt_number(self)
         total_jbolt_cost = jbolt_cost * jbolt_num
         return total_jbolt_cost
+
+    def get_jbolt_number(self):
+        return math.ceil(Order.get_tank_circumference(self) / 1.5)
 
     def calc_installation_cost(self):
         if self.diameter_tank <= 50:
@@ -94,3 +111,25 @@ class Order:
             return 22500
         else:
             return 0
+
+    def set_liner_discount(self, discounted_amount_percentage):
+        if discounted_amount_percentage > 0:
+            self.discounted = True
+        discounted_amount_number = discounted_amount_percentage / 100
+        self.discounted_single_liner_cost = Order.get_single_liner_cost(self) - (Order.get_single_liner_cost(self)
+                                          * discounted_amount_number)
+
+    def __get_liner_discount(self):
+        return self.discounted_single_liner_cost
+
+    def set_lifting_hem_cost(self):
+        self.lifting_hem_cost = Order.get_liner_circumference(self) * self.square_footage_price * 1.5
+
+    def get_lifting_hem_cost(self):
+        return self.lifting_hem_cost
+
+    def get_batten_strip_cost(self):
+        batten_strip_cost = 42
+        total_batten_strip_cost = batten_strip_cost * (Order.get_tank_circumference(self) / 8)
+        return total_batten_strip_cost
+
